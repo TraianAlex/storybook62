a2ca878ff9adc620236db4abf9be6a493233bf67
Traian Alexandru <victor_traian@yahoo.com>
2021-05-26 12:21:32 -0400

    design patterns

---
+++ src/stories/Design.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import DesignPatterns from '../DesignPattern/DesignPatterns';
+
+export default {
+  title: 'DesignPatterns/POC',
+  component: DesignPatterns,
+};
+
+const Template = (args) => <DesignPatterns {...args} />;
+
+export const ToDoFlux = Template.bind({});
