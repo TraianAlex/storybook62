08d8a94ae87334d6a242a4c355e63552fde03053
Traian Alexandru <victor_traian@yahoo.com>
2021-08-12 21:15:56 -0400

    add breadcrumbs

---
+++ src/Components/breadcrumbs/breadcrumbs.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Breadcrumbs from './breadcrumbs';
+
+export default {
+  title: 'Components',
+  component: Breadcrumbs,
+};
+
+const Template = (args) => <Breadcrumbs {...args} />;
+
+export const BreadcrumbsComp = Template.bind({});
